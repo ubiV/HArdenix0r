d9c650693063e57c5912c05660001eaa38a01737
Uberto Vittorio Favero <dev@ubertovittoriofavero.it>
2019-09-30 12:24:11 +0200

    fixed readme

--- main.py
+++ main.py
@@ -15,16 +15,26 @@ def getCompatibleCommand(c):
     #blah blabh blah FIXME
     return c['comp'][0]
 
+
+def executeCommand(command):
+    return os.system(command)
+def executeRemediation(node, resp):
+    if(node['response']['format'] == "text"):
+        if(resp != node['response']['text']):
+            return "Remediation Executed:", executeCommand(node['remediation'])
+        else:
+            return "No remediation needed"
+    if (node['response']['format'] == "json"):
+        print("text")
+    print(node)
+
+
 with open(SysInfo.commandFilePath) as json_file:
     data = json.load(json_file)
     for c in data['commands']:
         command = getCompatibleCommand(c)
         print("CHECKING: ",c['desc'])
+        resp = (executeCommand(command['command']))
+        executeRemediation(c, resp)
 
-
-def executeCommand():
-    print()
-
-def executeRemediation():
-    print()
 
